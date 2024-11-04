6083d94833e3b105f9338bd537a3e87b8dedf1d5
Tobias Rommel <tr099@hdm-stuttgart.de>
2024-11-04 20:26:13 +0100

    struktur

--- my-workspace/workshop-ui/src/app/app.component.ts
+++ my-workspace/workshop-ui/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'app-root',
-  template: `<app-nx-welcome></app-nx-welcome> <router-outlet></router-outlet>`,
+  template: `<router-outlet></router-outlet>`,
   styles: ``,
 })
 export class AppComponent {
